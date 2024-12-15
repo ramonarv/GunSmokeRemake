bdb7674c33eef7f1bc4220801302d87e1ded3373
ramonarv <supersplendid456@gmail.com>
2024-12-16 01:14:47 +0200

    fixed levelscrolling
    YES!!!!!

--- GunSmokeRemake/Assets/Scripts/LevelScroller.cs
+++ GunSmokeRemake/Assets/Scripts/LevelScroller.cs
@@ -5,29 +5,49 @@ using UnityEngine;
 
 public class LevelScroller : MonoBehaviour
 {
-    private Vector2 lastTile;
+    public Sprite sprite;
     public Transform[] tiles;
-    public int scrollSpeed;
+    public float scrollSpeed;
+    public float tileHeight;
+    public float pixelsPerUnit;
+
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        // getting transform of the LAST tile in the list
-        lastTile = new Vector2(tiles[tiles.Length - 1].position.x, tiles[tiles.Length -1].position.y);
-        Debug.Log(lastTile);
+        tileHeight = sprite.bounds.size.y;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         for (int i = 0; i < tiles.Length; i++)
         {
-            tiles[i].transform.Translate(Vector3.down * Time.deltaTime * scrollSpeed); 
-            if (tiles[i].transform.position.y < -9)
+
+            if (tiles[i].transform.position.y < -tileHeight)
             {
-                tiles[i].transform.position = lastTile;
-                Debug.Log(tiles[i].transform.position);
+                float highestY = GetHighestTileY();
+                tiles[i].transform.position = new Vector2(
+                    tiles[i].transform.position.x,
+                    highestY + tileHeight);
             }
+
+            tiles[i].transform.Translate(Vector2.down * Time.deltaTime * scrollSpeed);
         }
     }
+
+    float GetHighestTileY()
+    {
+        float highestY = tiles[0].transform.position.y;
+        for (int i = 1; i < tiles.Length; i++)
+        {
+            if (tiles[i].transform.position.y > highestY)
+            {
+                highestY = tiles[i].transform.position.y;
+            }
+        }
+        return highestY;
+    }
+
 }
